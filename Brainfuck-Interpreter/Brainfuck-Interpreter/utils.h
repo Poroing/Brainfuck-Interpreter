88e19383e5e3abedeb71400c713fc4b0d8331159
Poroing <jean-18-09@hotmail.fr>
2015-01-04 21:39:37 +0100

    Adjust mistaken push

--- Brainfuck-Interpreter/Brainfuck-Interpreter/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef BRAINFUCKCONSOLE_SRC_UTILS_H_
-#define BRAINFUCKCONSOLE_SRC_UTILS_H_
-
-#include <vector>
-#include <string>
-
-void split(std::vector<std::string>& v, std::string const& s, char del = ' ');
-
-#endif

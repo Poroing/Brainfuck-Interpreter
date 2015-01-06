4d2df434bac91ab60026788d874af04e131873ed
Poroing <jean-18-09@hotmail.fr>
2015-01-06 16:41:57 +0100

    Move files to arrage the code

--- Brainfuck-Interpreter/Brainfuck-Interpreter/include/args.h
+++
@@ -1,9 +0,0 @@
-#ifndef BRAINFUCKCONSOLE_SRC_ARGS_H_
-#define BRAINFUCKCONSOLE_SRC_ARGS_H_
-
-#include <string>
-#include "interpreter.h"
-
-void handle_args(BFInterpreter& interpreter, int argc, char* argv[]);
-
-#endif
